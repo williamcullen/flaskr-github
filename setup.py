35b5c6490ab9c9f08f6efe8d4d915b559b19c6c5
williamcullen <williamcullen@foxmail.com>
2017-07-04 17:34:13 +0800

    python-flask入门教程
    flaskr
    
    新闻客户端

---
+++ setup.py
@@ -0,0 +1,14 @@
+# _*_ encoding:utf-8 _*_
+__author__ = 'williamcullen'
+__date__ = '2017/7/4 16:34'
+
+from setuptools import setup
+
+setup(
+    name='flaskr',
+    packages=['flaskr'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
